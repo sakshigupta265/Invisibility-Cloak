3c46b34aab780a06badf19dd461bba7eeebb0eed
sakshigupta265 <itzsakshi26gupta@gmail.com>
2021-02-27 17:22:05 +0530

    Update background.py
    Added comments for better understanding and readability.

--- background.py
+++ background.py
@@ -1,15 +1,22 @@
-import cv2
+# capture the background
 
-camera = cv2.VideoCapture(0)
+import cv2  # import computer vision
+
+camera = cv2.VideoCapture(0) # initialise your camera
 
 while camera.isOpened():
-    ret, back = camera.read()
+    ret, back = camera.read()      
+    # ret: return true if camera is able to read
+    # back: gives the image read by the camera
 
     if ret:
-        cv2.imshow("background",back)
-        if cv2.waitKey(5) == ord('q'):
-            cv2.imwrite("background.jpg",back)
-            break
+        cv2.imshow("background",back) # show the input read by the camera
+        if cv2.waitKey(5) == ord('x'): 
+            # ord gives the unicode value of 'x'
+            # when x is pressed an image is captured and after a wait time of 5ms
 
-camera.release()
-cv2.destroyAllWindows
+            cv2.imwrite("background.jpg",back) # save the image captured
+            break 
+
+camera.release()    # release the camera (dislinking the camera)
+cv2.destroyAllWindows # close all windows
